c9b0178d3b54170079640a46b3402b60365ad4a0
Lacno, Crestine <crestinel123@gmail.com>
2025-09-04 20:32:47 +0800

    Update Exer1_Functional.java

--- EXER1_Lacno/Exer1_Functional.java
+++ EXER1_Lacno/Exer1_Functional.java
@@ -1,4 +1,3 @@
-
 public class Exer1_Functional {
     public static void main(String[] args) {
         //Double the numbers in an array and sum them
@@ -13,3 +12,4 @@ public class Exer1_Functional {
         System.out.println("Sum of doubled numbers: " + sum);
     }
 }
+
