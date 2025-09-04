51d8589c92ae0757232a7f059a8863975f764372
Lacno, Crestine <crestinel123@gmail.com>
2025-09-04 10:31:21 +0800

    test

--- EXER1_Lacno/Exer1_Functional.java
+++ EXER1_Lacno/Exer1_Functional.java
@@ -1,3 +1,10 @@
 public Class Exer1_Functional {
-    
+    public static void main(String[] args)  {
+
+        int sum = 20;
+
+        for(int i = 1; i <= sum; i++) {
+            if()
+        }
+    }
 }
